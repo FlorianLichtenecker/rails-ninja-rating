5ea2d72745b21d5338ba492afe1e126065003cf5
Florian Lichtenecker <florian.lichtenecker@gmail.com>
2023-08-29 16:17:11 +0200

    first draft of view + initial method in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def dashboard
-
+    review_platform_ids = UserReviewPlatform.where(user_id: current_user.id).pluck(:review_platform_id)
+    @review_platforms = ReviewPlatform.where(id: review_platform_ids)
   end
 end
