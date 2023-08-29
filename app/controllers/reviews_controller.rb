f3049738bd140c9843e2e6643963a7983ff7ae0b
Mateo Campins <mateo.campins@gmail.com>
2023-08-29 17:59:59 +0200

    Not finished

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,10 @@
 class ReviewsController < ApplicationController
+
+  def manage
+    @open_reviews = Review.where(status: 'open').order(date: :asc)
+    @current_review = @open_reviews.first
+  end
+
+  def next_review
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "users#dashboard"
+  get 'reviews/manage', to: 'reviews#manage'
+  get 'reviews/next_review', to: 'reviews#next_review', as: 'next_review'
 end
