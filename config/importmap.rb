17b28a4552e36c9f4d3b364ebb5e66b488e9145e
Florian Lichtenecker <florian.lichtenecker@gmail.com>
2023-09-05 11:32:09 +0200

    implemented flatpicker, date filter functionality not yet functioning

--- config/importmap.rb
+++ config/importmap.rb
@@ -10,3 +10,4 @@ pin "@popperjs/core", to: "popper.js", preload: true
 pin "chartkick", to: "chartkick.js"
 pin "Chart.bundle", to: "Chart.bundle.js"
 pin "select2", to: "https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js", preload: true
+pin "flatpickr", to: "https://ga.jspm.io/npm:flatpickr@4.6.13/dist/esm/index.js"
